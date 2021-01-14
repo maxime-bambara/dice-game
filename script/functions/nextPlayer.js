f0033f78240643843b1f67937aa1038b64966dab
maxime bambara <maxime.f.bambara@gmail.com>
2021-01-14 12:18:59 +0100

    Refactoring newGame function and add a select player span icon

--- script/functions/nextPlayer.js
+++ script/functions/nextPlayer.js
@@ -3,8 +3,12 @@ function nextPlayer(){
     if (activePlayer === 0){
         activePlayer = 1
         resetScores()
+        document.getElementById('select-player-1').style.opacity = '0';
+        document.getElementById('select-player-2').style.opacity= '0.8';
     } else{
         activePlayer = 0
         resetScores()
+        document.getElementById('select-player-2').style.opacity = '0';
+        document.getElementById('select-player-1').style.opacity= '0.8';
     }
 }
--- script/script.js
+++ script/script.js
@@ -1,22 +1,6 @@
 // Initialisation des variables
 let scores, activePlayer, diceScore, inGame, currentScores, playerNumber
 
-// Remettre à zéro les variables pour une nouvelle partie
-function newGame(){
-    scores = [0,0]
-    currentScores = [0,0]
-    activePlayer = 0
-    diceScore = 0
-    inGame = true
-    playerNumber = [1,2]
-
-    document.getElementById('img-result').src = 'assets/img/0.jpg'
-    document.getElementById('score-player-1').textContent = 0
-    document.getElementById('score-player-2').textContent = 0
-    document.getElementById('dice-result-1').textContent = 0
-    document.getElementById('dice-result-2').textContent = 0
-}
-
 // Récupération du boutton 'Nouvelle partie'
 document.getElementById('new-game').addEventListener('click', newGame)
 
