bed4b8efa51df7398bb43e1888d5b685306035dc
maxime bambara <maxime.f.bambara@gmail.com>
2021-01-15 18:40:19 +0100

    Fix end game bug

--- script/functions/checkIfWin.js
+++ script/functions/checkIfWin.js
@@ -1,7 +1,7 @@
 // Vérifier si le joueur a gagner et changer de joueur si ce n'est pas le cas
 function checkIfWin() {
 
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 10) {
         alert('Victoire du joueur ' + playerNumber[activePlayer]);
         newGame();
         return true;
